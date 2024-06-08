86d6df2e5551fa6003eb8b2b29b64056ca14436c
flaviostutz <flaviostutz@gmail.com>
2024-06-08 22:55:43 +0200

    chore: first working version tested with openai public api

--- lib/src/workspacePromptRunner.ts
+++ lib/src/workspacePromptRunner.ts
@@ -36,6 +36,10 @@ export type SendAndProcessPromptArgs = {
    */
   model: openai.ChatModel;
   /**
+   * Maximum number of tokens allowed to be sent to the API in a single request
+   */
+  maxTokens?: number;
+  /**
    * OpenAI completion session to be used for generating code
    * Used to track the conversation state.
    * If defined, "model" param is ignored.
@@ -49,7 +53,7 @@ export type SendAndProcessPromptArgs = {
  */
 export type WorkspacePromptRunnerArgs = Pick<
   SendAndProcessPromptArgs,
-  'outputDir' | 'openAIClient' | 'model'
+  'outputDir' | 'openAIClient' | 'model' | 'maxTokens'
 > & {
   /**
    * Arguments for generating the code prompt
@@ -70,6 +74,7 @@ export const workspacePromptRunner = async (args: WorkspacePromptRunnerArgs): Pr
     outputDir: args.outputDir,
     openAIClient: args.openAIClient,
     model: args.model,
+    maxTokens: args.maxTokens,
   });
 };
 
@@ -82,6 +87,10 @@ const sendAndProcessWorkspacePrompt = async (
   totalFilePaths?: string[],
 ): Promise<string[]> => {
   let selfFilePaths = totalFilePaths;
+
+  // track how many times additional files were requested
+  let additionalFilesCounter = 0;
+
   // initialize on first call
   if (!selfFilePaths) {
     selfFilePaths = [];
@@ -91,10 +100,13 @@ const sendAndProcessWorkspacePrompt = async (
   if (!chatSession) {
     console.log('Creating new chat session');
     chatSession = createOpenAICompletionSession(args.openAIClient, {
-      model: args.model,
-      temperature: 0, // make the output more deterministic amongst calls
-      seed: 0, // make the output more deterministic amongst calls
-      topP: 0.95,
+      openaiConfig: {
+        temperature: 0, // make the output more deterministic amongst calls
+        seed: 0, // make the output more deterministic amongst calls
+        top_p: 0.95,
+        model: args.model,
+      },
+      maxConversationTokens: args.maxTokens,
     });
   }
   console.log(`>> Sending prompt: ${args.prompt}`);
@@ -111,8 +123,14 @@ const sendAndProcessWorkspacePrompt = async (
     outputFiles.forEach((file) => {
       const filePath = path.join(args.outputDir, file.filename);
 
-      console.log(`Writing generated file: ${filePath}`);
+      console.log(`Writing generated file: ${file.filename}`);
+
+      // create folder if it doesn't exist
+      fs.mkdirSync(path.dirname(filePath), { recursive: true });
+
+      // write file (replace if exists)
       fs.writeFileSync(filePath, file.contents);
+
       if (typeof selfFilePaths === 'undefined') {
         throw new Error('selfFilePaths shouldnt be undefined');
       }
@@ -132,6 +150,10 @@ const sendAndProcessWorkspacePrompt = async (
 
     // FILES REQUESTED
   } else if (responseOutput.indexOf('outcome: files-requested') > -1) {
+    additionalFilesCounter += 1;
+    if (additionalFilesCounter > 1) {
+      throw new Error('files-requested outcome loop detected. Aborting.');
+    }
     const requestedFileList = parseFileList(responseOutput);
 
     const sendFilesRegexes = requestedFileList
@@ -143,11 +165,16 @@ const sendAndProcessWorkspacePrompt = async (
       fullContentsRegexes: sendFilesRegexes,
     });
 
+    let prompt = requestedFilesPrompt.fullFileContents;
+    if (!prompt) {
+      prompt = 'Proceed without additional files';
+    }
+
     selfFilePaths = await sendAndProcessWorkspacePrompt(
       {
         ...args,
         openAICompletionSession: chatSession,
-        prompt: requestedFilesPrompt.fullFileContents,
+        prompt,
       },
       selfFilePaths,
     );
