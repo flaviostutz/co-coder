8eef7faa9bde63884b75e17f68bce563f172b599
flaviostutz <flaviostutz@gmail.com>
2024-06-11 21:29:54 +0200

    feat: adding configurations to cli

--- lib/src/main.ts
+++ lib/src/main.ts
@@ -4,14 +4,14 @@ import { run } from './cli';
 
 // eslint-disable-next-line no-void
 void (async (): Promise<void> => {
-  process.on('uncaughtException', (err) => {
-    const errs = `${err}`;
-    let i = errs.indexOf('\n');
-    if (i === -1) i = errs.length;
-    // eslint-disable-next-line no-console
-    console.log(errs.substring(0, i));
-    process.exit(3);
-  });
+  // process.on('uncaughtException', (err) => {
+  //   const errs = `${err}`;
+  //   let i = errs.indexOf('\n');
+  //   if (i === -1) i = errs.length;
+  //   // eslint-disable-next-line no-console
+  //   console.log(errs.substring(0, i));
+  //   process.exit(3);
+  // });
   const exitCode = await run(process.argv);
   process.exit(exitCode);
 })();
